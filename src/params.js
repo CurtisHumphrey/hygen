46f64c460f0e8686fed28bc8dcf2726d4283eb1f
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2019-03-04 08:34:33 -0600

    feat: added flag to have it prompt for generator and action

--- src/params.js
+++ src/params.js
@@ -6,16 +6,40 @@ const L = require('lodash')
 const path = require('path')
 const yargs = require('yargs-parser')
 const prompt = require('./prompt')
+const { availableActions } = require('./help')
 
 const params = async (
-  { templates, createPrompter }: RunnerConfig,
+  { templates, logger, createPrompter }: RunnerConfig,
   externalArgv: Array<string>
 ): any => {
   const argv = yargs(externalArgv)
+  let [generator, action] = argv._
 
-  const [generator, action] = argv._
+  if (argv.prompt) {
+    logger.log('Asking for generator and action')
+
+    const availableOptions = availableActions(templates)
+
+    const prompter = createPrompter()
+    let response = await prompter.prompt({
+      type: 'select',
+      name: 'generator',
+      message: 'Generator?',
+      choices: Object.keys(availableOptions)
+    })
+    generator = response.generator
+
+    response = await prompter.prompt({
+      type: 'select',
+      name: 'action',
+      message: 'Action?',
+      choices: availableOptions[generator]
+    })
+    action = response.action
+  }
+
   if (!generator || !action) {
-    return { generator, action, templates }
+    return Object.assign({ generator, action, templates })
   }
   const [mainAction, subaction] = L.split(action, ':')
 
